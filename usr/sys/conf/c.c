d11c788d45368cf769bc0094faae9023ae6332a7
Robert Lipe <robertlipe@gmail.com>
2020-12-20 17:30:57 -0600

    Cleanups in sys

--- usr/sys/conf/c.c
+++ usr/sys/conf/c.c
@@ -19,17 +19,26 @@ __attribute__ ((aligned (16))) char mscratch0[32 * NCPU];
 
 int	nulldev();
 int	nodev();
+int	hdopen(), hdstrategy();
+struct	buf	hdtab;
+int	mdstrategy();
+struct	buf	mdtab;
 struct	bdevsw	bdevsw[] =
 {
-	nodev, nodev, nodev, 0, /* hd = 0 */
+	hdopen,  nulldev, hdstrategy, &hdtab,	/* hd = 0 */
 	nodev, nodev, nodev, 0, /* fd = 1 */
-	nodev, nodev, nodev, 0, /* md = 2 */
+	nulldev, nulldev, mdstrategy, &mdtab, 	/* md = 2 */
 	0
 };
 
 int	scopen(), scclose(), scread(), scwrite(), scioctl();
 int	mmread(), mmwrite();
 int	syopen(), syread(), sywrite(), sysioctl();
+int	serialopen(), serialclose(), serialread(), serialwrite(), serialioctl();
+struct	tty	serial[1];
+int	hdread(), hdwrite();
+int	mdread(), mdwrite();
+int	cdread();
 
 struct	cdevsw	cdevsw[] =
 {
@@ -37,11 +46,11 @@ struct	cdevsw	cdevsw[] =
 	nulldev, nulldev, mmread, mmwrite, nodev, nulldev, 0, 	/* mem = 1 */
 	syopen, nulldev, syread, sywrite, sysioctl, nulldev, 0,	/* tty = 2 */
 	nodev, nodev, nodev, nodev, nodev, nulldev, 0, /* sr = 3 */
-	nodev, nodev, nodev, nodev, nodev, nulldev, 0, /* serial = 4 */
-	nodev, nodev, nodev, nodev, nodev, nulldev, 0, /* hd = 5 */
+	serialopen, serialclose, serialread, serialwrite, serialioctl, nulldev, serial,	/* serial = 4 */
+	hdopen, nulldev, hdread, hdwrite, nodev, nulldev, 0,	/* hd = 5 */
 	nodev, nodev, nodev, nodev, nodev, nulldev, 0, /* fd = 6 */
-	nodev, nodev, nodev, nodev, nodev, nulldev, 0, /* md = 7 */
-	nodev, nodev, nodev, nodev, nodev, nulldev, 0, /* cd = 8 */
+	nulldev, nulldev, mdread, mdwrite, nodev, nulldev, 0,	/* md = 7 */
+	nulldev, nulldev, cdread, nodev, nodev, nulldev, 0,	/* cd = 8 */
 	0
 };
 
@@ -51,12 +60,12 @@ struct	linesw	linesw[] =
 	ttyopen, nulldev, ttread, ttwrite, nodev, ttyinput, ttstart, /* 0 */
 	0
 };
-dev_t	rootdev	= makedev(-1, 0);
-dev_t	swapdev	= makedev(-1, 0);
-dev_t	pipedev = makedev(-1, 0);
+dev_t	rootdev	= makedev(2, 0);
+dev_t	swapdev	= makedev(2, 0);
+dev_t	pipedev = makedev(2, 0);
 int	nldisp = 1;
-daddr_t	swplo	= 4000;
-int	nswap	= 872;
+daddr_t	swplo	= 2880;
+int	nswap	= 32000;
 	
 struct	buf	buf[NBUF];
 struct	file	file[NFILE];
