f7742bc318d1d22d22f9f54587aa7e4196ee095e
Robert Lipe <robertlipe@gmail.com>
2021-02-10 22:45:27 -0600

    Use symbol wrapprs to ensure alignment, debugging symbols for all our system call entry wrappers.

--- usr/sys/h/riscv-asm.h
+++ usr/sys/h/riscv-asm.h
@@ -13,3 +13,16 @@
 #define	ENTRY(sym) \
 	.text; .globl sym; .type sym,@function; .align 4; sym: .cfi_startproc;
 #define	END(sym) .cfi_endproc; .size sym, . - sym
+
+// Ugh. user-space should not include this file at all, but it does, so here is
+// a userspace thing. I *think* it is safe to clobber a1 here to do a.
+// Worse, we have to rely on an obscure feature of GAS. Since we're #defining
+// and string continuing, our line numbers never go up so you can't branch to
+// '1f:' when you're o the same line. Fortunately '\@' is exactly the ticket.
+
+#define SET_ERRNO \
+	beqz a0, \@f ; \
+	la	a1, _errno; \
+	sw	a1, (a1); \
+	li 	a0, -1;
+\@f:	ret
